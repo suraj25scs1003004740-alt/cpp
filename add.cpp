94d0369dc6e751a5e947ce5b27281d66d764e94e
SURAJ PRASAD <suraj.25scs1003004740@iilm.edu>
2026-08-06 23:17:46 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ int main(){
+   int x=10;
+   float y=100.344;
+   x=x+1;
+   cout<<x<<endl;
+   x+=1;
+   cout<<x<<endl;
+   cout<<x++<<endl;//here value is printed then incresed
+   cout<<x--<<endl;
+   cout<<++x<<endl;//here value is incresed then printed
+   cout<<--x<<endl;
+   cout<<x+12<<endl;
+   cout<<x+y<<endl;
+
+    return 0;
+ 
+ }
